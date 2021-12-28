899d05c9578dea499338f27128e192939185aff3
sarahc987 <89328572+sarahc987@users.noreply.github.com>
2021-12-29 08:35:43 +1000

    Created react app and cleaned up files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +20,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
