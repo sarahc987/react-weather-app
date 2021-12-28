f549fc48d9b7b3220be87174d064f1463f58ff65
sarahc987 <89328572+sarahc987@users.noreply.github.com>
2021-12-29 08:48:58 +1000

    Added github repository URL

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      Weather App
+      <footer>
+        This project was coded by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/sarah-cox-127b6b134/"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Sarah Cox
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/sarahc987/react-weather-app/commit/899d05c9578dea499338f27128e192939185aff3"
+          target="_blank"
+        >
+          open-sourced on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
