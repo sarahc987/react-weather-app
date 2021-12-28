6561bb82d59bde67b6028876f8cbac2e9b4a0d37
sarahc987 <89328572+sarahc987@users.noreply.github.com>
2021-12-29 09:38:04 +1000

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/sarah-cox-127b6b134/"
-          target="_blank"
-        >
-          Sarah Cox
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/sarahc987/react-weather-app/commit/899d05c9578dea499338f27128e192939185aff3"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/sarah-cox-127b6b134/"
+            target="_blank"
+          >
+            Sarah Cox
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/sarahc987/react-weather-app/commit/899d05c9578dea499338f27128e192939185aff3"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
